8b7e45630f2901f7194d51863f1c6016ae5db13a
ShadowWormNiezadowolony <135991611+ShadowWormNiezadowolony@users.noreply.github.com>
2025-06-25 01:12:05 +0200

    Poprawka używalności banera

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,13 +2,14 @@ import { Baner } from "@/components/Baner";
 import { Menu } from "@/components/Menu";
 import { CATEGORY_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch } from "@/sanity/lib/live";
+import { NavBar } from "@/components/NavBar";
 
 export default async function Page() {
   const { data: Products } = await sanityFetch({ query: CATEGORY_QUERY });
 
   return (
     <main>
-      <Baner kategorie={Products.categories} />
+      <Baner><NavBar Kategorie={Products.categories} /></Baner>
       <Menu dane={Products.products} kategorie={Products.categories} />
     </main>
   );
--- src/components/Baner.tsx
+++ src/components/Baner.tsx
@@ -1,10 +1,10 @@
-import { SanityAsset } from "@sanity/image-url/lib/types/types";
-import { NavBar } from "./NavBar";
-
-export function Baner({kategorie} : {kategorie:SanityAsset}) {
+export function Baner({ children }: Readonly<{ children?: React.ReactNode }>) {
   return (
-      <div id="baner" className="bg-[url(/baner.png)] w-full h-[50vh] md:h-[40vh] bg-no-repeat bg-cover bg-center flex items-baseline-last">
-                <NavBar Kategorie={kategorie} />
-      </div>
-  )
+    <div
+      id="baner"
+      className="bg-[url(/baner.png)] w-full h-[50vh] md:h-[40vh] bg-no-repeat bg-cover bg-center flex items-baseline-last"
+    >
+      {children}
+    </div>
+  );
 }
